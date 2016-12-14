6c5382e37fc18aecfbfb688f2969344182288500
Richard Huang <flyerhzm@gmail.com>
2016-12-14 09:32:21 +0800

    remove created_by and updated_by

--- lib/sequel/plugins/auditable/instance_methods.rb
+++ lib/sequel/plugins/auditable/instance_methods.rb
@@ -1,24 +1,6 @@
 module Sequel::Plugins
   module Auditable
     module InstanceMethods
-      def created_by
-        first_audit = Audit.where(resource_id: self.id, resource_type: self.class.name).order(:created_at).first
-        if first_audit
-          { id: first_audit.user_id, name: first_audit.user_name }
-        else
-          {}
-        end
-      end
-
-      def updated_by
-        last_audit = Audit.where(resource_id: self.id, resource_type: self.class.name).order(:created_at).last
-        if last_audit
-          { id: last_audit.user_id, name: last_audit.user_name }
-        else
-          {}
-        end
-      end
-
       def before_create
         super
         self.created_by = current_user_id if self.respond_to? :created_by=
@@ -26,6 +8,7 @@ module Sequel::Plugins
 
       def after_save
         super
+        self.updated_by = current_user_id if self.respond_to? :updated_by=
         fields = model.additional_fields.inject({}) { |hash, field| hash[field] = self.send(field); hash }
         Audit.create(user_id: current_user_id, user_name: current_user_name, resource: self, fields: Sequel.pg_json(fields))
       end
