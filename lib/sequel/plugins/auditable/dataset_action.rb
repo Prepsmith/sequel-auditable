41f7be205667cb49b2fd2270a711f4e67a75aff0
Richard Huang <flyerhzm@gmail.com>
2017-05-26 20:05:39 +0800

    add updated_by_id when inserting

--- lib/sequel/plugins/auditable/dataset_action.rb
+++ lib/sequel/plugins/auditable/dataset_action.rb
@@ -2,16 +2,27 @@ module Sequel::Plugins
   module Auditable
     module AddCreatedById
       def insert_sql(*values)
-        if self.columns.include?(:created_by_id) && values.length == 1 && values.first.is_a?(Hash) && Thread.current[:current_user]
-          super(values.first.merge(created_by_id: Thread.current[:current_user].user_id))
+        if values.length == 1 && values.first.is_a?(Hash) && Thread.current[:current_user]
+          options = values.first
+          options.merge!(created_by_id: Thread.current[:current_user].user_id) if self.columns.include?(:created_by_id)
+          options.merge!(updated_by_id: Thread.current[:current_user].user_id) if self.columns.include?(:updated_by_id)
+          super(options)
         else
           super
         end
       end
 
       def multi_insert_sql(columns, values)
-        if self.columns.include?(:created_by_id) && Thread.current[:current_user]
-          super(columns << :created_by_id, values.map { |v| v << Thread.current[:current_user].user_id })
+        if Thread.current[:current_user]
+          if self.columns.include?(:created_by_id)
+            columns << :created_by_id
+            values.each { |v| v << Thread.current[:current_user].user_id }
+          end
+          if self.columns.include?(:updated_by_id)
+            columns << :updated_by_id
+            values.each { |v| v << Thread.current[:current_user].user_id }
+          end
+          super(columns, values)
         else
           super
         end
